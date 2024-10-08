a2b43ffab6fb9f42b9571f88b0e8b5affdf3f2a0
passer-by626 <93714751+passer-by626@users.noreply.github.com>
2024-10-08 10:08:02 +0800

    feat: 项目初始化

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<!--
+ * @Author: liz
+ * @Date: 2024-10-08 09:43:03
+ * @LastEditTime: 2024-10-08 09:50:59
+ * @LastEditors: liz
+ * @Description: 
+ * @FilePath: \large-screen\src\App.vue
+-->
+<template>
+  <router-view />
+</template>
+
+<style lang="scss">
+@import "./assets/reset.scss";
+#app {
+  width: 100vw;
+  height: 100vh;
+  overflow: hidden;
+}
+</style>
---
+++ src/views/Home/index.vue
@@ -0,0 +1,39 @@
+<!--
+ * @Author: liz
+ * @Date: 2024-10-08 09:43:03
+ * @LastEditTime: 2024-10-08 09:54:23
+ * @LastEditors: liz
+ * @Description: 
+ * @FilePath: \large-screen\src\views\HomeView.vue
+-->
+<template>
+  <div class="container">
+    <iframe src="https://www.zjjnks.cn/swat/hangzhou/" frameborder="0"></iframe>
+    <iframe src="https://www.zjjnks.cn/swat/hangzhou/" frameborder="0"></iframe>
+    <iframe src="https://www.zjjnks.cn/swat/hangzhou/" frameborder="0"></iframe>
+    <iframe src="https://www.zjjnks.cn/keyunit" frameborder="0"></iframe>
+    <iframe src="https://www.zjjnks.cn/keyunit" frameborder="0"></iframe>
+    <iframe src="https://www.zjjnks.cn/keyunit" frameborder="0"></iframe>
+    <iframe src="https://www.zjjnks.cn/keyArea/" frameborder="0"></iframe>
+    <iframe src="https://www.zjjnks.cn/keyArea/" frameborder="0"></iframe>
+    <iframe src="https://www.zjjnks.cn/keyArea/" frameborder="0"></iframe>
+  </div>
+</template>
+
+<script setup></script>
+
+<style lang="scss" scoped>
+.container {
+  width: 100%;
+  height: 100%;
+  display: grid;
+  grid-template-columns: repeat(3, 1fr);
+  grid-template-rows: repeat(3, 1fr);
+  grid-column-gap: 0px;
+  grid-row-gap: 0px;
+  iframe {
+    width: 100%;
+    height: 100%;
+  }
+}
+</style>
